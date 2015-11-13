4e64942795202157732b1d7cbb5338442d745e1c
Ben Allred <skuttleman@gmail.com>
2015-11-13 15:55:49 -0700

    Ground Zero

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var main = require('../src/script/main.js'),
   expect = require('chai').expect;
 
 describe('Test Suite for: wordsearch', function(){
-	it('should be configured to write tests', function() {
-    expect(main.add(1, 2)).to.equal(3);
+	it('', function() {
+    
 	});
 });
