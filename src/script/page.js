b2963e425b8928c7bb3831da1c9a999be3426de6
Ben Allred <skuttleman@gmail.com>
2015-11-18 04:29:31 -0700

    Definition displays in modal window

--- src/script/page.js
+++ src/script/page.js
@@ -1,5 +1,5 @@
 var mainPuzzle, dragTrack = {}, minWordCount = 15, maxWordCount=50,
-  menuPosition = '-100vh';
+  menuDisplayed = false, modalCallback;
 
 function drawGrid(grid) {
   $('.puzzle').remove();
@@ -11,13 +11,15 @@ function drawWordList(key) {
   $('.word-list').remove();
   loadStub({ parent: '.word-list-container', file: './stubs/word_list.html',
     params: { key: key } }, function() {
+      $('.words').click(showDefinition);
       for (var i = 0; i < key.length; i ++) {
         if (key[i].selected) {
           highlightCells({ vector: key[i], mode: 'add',
             classes: ['selected'] });
         }
       }
-    });
+    }
+  );
 }
 
 function drawPuzzle(puzzle) {
@@ -26,16 +28,9 @@ function drawPuzzle(puzzle) {
   saveLocal();
 }
 
-// function showOptions() {
-//   $('.puzzle-options').removeClass('hide');
-//   $('.start').addClass('hide');
-// }
-
 function getPuzzleRowCol(x, y) {
   var $puzzle = $('.puzzle');
   var $puzzleCorner = $('.puzzle-cell--0-0');
-  // console.log($puzzle.position());
-  // console.log(x, y);
   var row = Math.floor(((y - $puzzleCorner.position().top) /
     ($puzzleCorner.height() * mainPuzzle.puzzle.grid.length)) *
     mainPuzzle.puzzle.grid.length);
@@ -60,18 +55,26 @@ function loadStub(params, callback) {
   fitPuzzle();
 }
 
+function hideMenu() {
+  $('.menu-container').animate({ top: '-100vh' }, 400);
+  menuDisplayed = false;
+}
+
+function showMenu() {
+  $('.menu-container').animate({ top: '50px' }, 400);
+  menuDisplayed = true;
+}
+
 $(function() {
   $('.menu-button').click(function(event) {
-    var $menu = $('.menu-container');
-    if (menuPosition === '50px') menuPosition = '-100vh';
-    else menuPosition = '50px';
-    $menu.animate({ top: menuPosition }, 400);
+    event.preventDefault();
+    if (menuDisplayed) window.hideMenu();
+    else window.showMenu();
   });
 
   var load = localStorage.wordsearch;
   if (load) {
     mainPuzzle = JSON.parse(load);
-    // mainPuzzle.prototype = new WordSearch();
     drawPuzzle(mainPuzzle.puzzle);
   } else {
     $('.menu-button').click();
@@ -89,7 +92,8 @@ $(function() {
     var numWords = parseInt($('.num-words').val());
 
     if (isNaN(numWords) || numWords < minWordCount || numWords > maxWordCount) {
-      // throwInvalid(); -->>
+      popUp('The number of words must be a number between ' + minWordCount +
+      ' and ' + maxWordCount + '!');
     } else {
       $('.puzzle-container').html('<p class="puzzle">Loading new puzzle...</p>');
       menuPosition = '-100vh';
@@ -99,17 +103,18 @@ $(function() {
         });
       });
     }
-  })
-////////////////////////
-////////////////////////
-/// FOR TESTING ONLY ///
-////////////////////////
-////////////////////////
-  // .click();
+  });
 
   $('.puzzle-options').on('keypress', function(event){
     if (event.keyCode === 13) event.preventDefault();
     $('.submit-form')[0].click();
+  });
+
+  $('.modal-button').on('click', function(event) {
+    event.preventDefault();
+    $('.modal-container').hide();
+    if (modalCallback) modalCallback();
+    modalCallback = null;
   });
 });
 
@@ -131,11 +136,14 @@ $(document).on('mouseup touchend', function(event) {
 });
 
 function chomp(event) {
-  var coordinates = Array.prototype.filter.call(event.target.classList, function(element) {
+  var start = Array.prototype.filter.call(event.target.classList, function(element) {
     return element.indexOf('--') + 1;
-  })[0].split('--')[1].split('-');
-  return coordinates.length === 2 ? { row: parseInt(coordinates[0]),
-    col: parseInt(coordinates[1]) } : {};
+  })[0];
+  if (start) {
+    var coordinates = start.split('--')[1].split('-');
+    return coordinates.length === 2 ? { row: parseInt(coordinates[0]),
+      col: parseInt(coordinates[1]) } : {};
+  }
 }
 
 function puzzleDown(event) {
@@ -147,7 +155,7 @@ function puzzleDown(event) {
 
 function puzzleDrag(event) {
   event.preventDefault();
-  if (dragTrack.start) {
+  if (dragTrack && dragTrack.start) {
     // dragTrack.end = getPuzzleRowCol(event.pageX, event.pageY);
 
     dragTrack.end = event.originalEvent.touches ?
@@ -166,7 +174,7 @@ function puzzleDrag(event) {
 
 function puzzleUp(event){
   event.preventDefault();
-  if (dragTrack.start && dragTrack.end) {
+  if (dragTrack && dragTrack.start && dragTrack.end) {
     var key = mainPuzzle.puzzle.key;
     for (var i = 0; i < key.length; i ++) {
       if (key[i].start.row === dragTrack.start.row && key[i].start.col ===
@@ -181,6 +189,7 @@ function puzzleUp(event){
           highlightCells({ vector: dragTrack, classes: ['selected'],
             mode: 'add' });
           i = key.length;
+          isPuzzleFinished();
       }
     }
   }
@@ -193,27 +202,27 @@ function highlightCells(params) {
     }
   } else {
     var cellList = makeCellList(params.vector);
-    if (cellList.length === 0) return {};
-    var ret = { start: {}, end: {} };
-    ret.start.row = cellList[0][0];
-    ret.start.col = cellList[0][1];
-    ret.end.row = cellList[cellList.length - 1][0];
-    ret.end.col = cellList[cellList.length - 1][1];
-    for (i = 0; i < params.classes.length; i ++) {
-      for (var j = 0; j < cellList.length; j ++) {
-        var row = cellList[j][0];
-        var col = cellList[j][1];
-        if (params.mode === 'add') {
-          console.log('.puzzle-cell--' + row + '-' + col);
-          console.log(params.classes[i]);
-          $('.puzzle-cell--' + row + '-' + col).addClass(params.classes[i]);
-        } else {
-          $('.puzzle-cell--' + row + '-' + col).removeClass(params.classes[i]);
+    if (cellList) {
+      if (cellList.length === 0) return {};
+      var ret = { start: {}, end: {} };
+      ret.start.row = cellList[0][0];
+      ret.start.col = cellList[0][1];
+      ret.end.row = cellList[cellList.length - 1][0];
+      ret.end.col = cellList[cellList.length - 1][1];
+      for (i = 0; i < params.classes.length; i ++) {
+        for (var j = 0; j < cellList.length; j ++) {
+          var row = cellList[j][0];
+          var col = cellList[j][1];
+          if (params.mode === 'add') {
+            $('.puzzle-cell--' + row + '-' + col).addClass(params.classes[i]);
+          } else {
+            $('.puzzle-cell--' + row + '-' + col).removeClass(params.classes[i]);
+          }
         }
       }
     }
+    return ret;
   }
-  return ret;
 }
 
 function makeCellList(vector) {
@@ -222,7 +231,7 @@ function makeCellList(vector) {
     else if (a === b) return 0;
     else return 1;
   }
-
+  if (!vector.start || !vector.end) return;
   var verticalDist = Math.abs(Math.abs(vector.start.row) -
     Math.abs(vector.end.row));
   var horizontalDist = Math.abs(Math.abs(vector.start.col) -
@@ -244,4 +253,25 @@ function makeCellList(vector) {
 
 function saveLocal() {
   localStorage.wordsearch = JSON.stringify(mainPuzzle);
+}
+
+function isPuzzleFinished() {
+  for (var i = 0; i < mainPuzzle.puzzle.key.length; i ++) {
+    if (!mainPuzzle.puzzle.key[i].selected) return;
+  }
+  localStorage.clear();
+  popUp('Congratulations! You finished this puzzle', showMenu);
+}
+
+function popUp(message, callback) {
+  $('.modal-message').html(message);
+  $('.modal-container').show();
+  $('.modal-button').focus();
+  modalCallback = callback;
+}
+
+function showDefinition(event) {
+  var word = event.target.innerText;
+  var definition = WordSearch.prototype.whatIsDefinition.call(mainPuzzle, word);
+  popUp(definition);
 }
